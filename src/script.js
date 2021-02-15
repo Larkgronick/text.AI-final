c9fa32e1b5378363973319bb411cbc91ca78ea9b
Nikita Zhavoronkov <larkgronick@gmail.com>
2021-02-15 21:22:40 +0300

    feat: check send to the button #3

--- src/script.js
+++ src/script.js
@@ -36,7 +36,6 @@ selectButton.onclick = (e) => {
 //open result
 checkButton.onclick = (e) => {
   e.preventDefault();
-  console.log(result);
   let action = selectButton.textContent.substring(1);
   if(action === 'Uniqueness'){
     scanAnimation.classList.add('active');
@@ -68,18 +67,18 @@ function showResult(){
   checkArea.style.display = 'none';
 }
 
-// function animateValue(id, start, end, duration) {
-//   if (start === end) return;
-//   let range = end - start;
-//   let current = start;
-//   let increment = end > start? 1 : -1;
-//   let stepTime = Math.abs(Math.floor(duration / range));
-//   let obj = document.getElementById(id);
-//   let timer = setInterval(function() {
-//       current += increment;
-//       obj.innerHTML = current;
-//       if (current == end) {
-//           clearInterval(timer);
-//       }
-//   }, stepTime);
-// }
+function animateValue(id, start, end, duration) {
+  if (start === end) return;
+  let range = end - start;
+  let current = start;
+  let increment = end > start? 1 : -1;
+  let stepTime = Math.abs(Math.floor(duration / range));
+  let obj = document.getElementById(id);
+  let timer = setInterval(function() {
+      current += increment;
+      obj.innerHTML = current;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+}
